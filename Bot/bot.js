675be44e1ab84a85f346c29fa44028e0073dc3a5
Suleyman Myradov <102394611+suleymanmyradov@users.noreply.github.com>
2024-04-04 18:04:05 +0500

    Update bot.js

--- Bot/bot.js
+++ Bot/bot.js
@@ -2,7 +2,7 @@ const { Telegraf } = require("telegraf");
 const TOKEN = "6596372383:AAHQWDvOkot3doQSCzT2laKU9jvyeFfunUo";
 const bot = new Telegraf(TOKEN);
 
-const web_link = "https://celebrated-torte-184681.netlify.app/";
+const web_link = "https://suleymanmyradov.github.io/durger/";
 
 bot.start((ctx) =>
   ctx.reply("Welcome :)))))", {
